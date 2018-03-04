22c2075f04230adc80a1b5accf3ef6576cba6568
Eduardo Nieto <en.castellano@gmail.com>
2018-03-05 00:09:53 +0100

    add data and zoom

--- js/flightanimation.js
+++ js/flightanimation.js
@@ -1,4 +1,5 @@
 var isRunning = true;
+var isZooming = false;
 
 $(function () {
  
@@ -10,9 +11,13 @@ $(function () {
   var currentWidth = $('#map').width();
   var width = 960;
   var height = 780;
+  var centered,
+    planes,
+    routes,
+    countries,
+    airports;
 
   var relationSize = width / height;
-
   var projection = d3.geo
     .albers();
 
@@ -24,25 +29,45 @@ $(function () {
     .path()
     .pointRadius(2)
     .projection(projection);
-
-
+  
+  var zoom = d3.behavior.zoom()
+    .translate(projection.translate())
+    .scale(projection.scale())
+    .scaleExtent([height, 8 * height])
+    .on("zoom", zoomed);
+  
+  
   var svg = d3.select("#map")
     .append("svg")
     .attr("viewBox", "0 0 " + width + " " + height)
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .call(zoom);
 
-
-  var airportMap = {};
-
-  var extant = [];
-  var flights = crossfilter(),
+  //   tip = d3.tip()
+  //   .attr('class', 'd3-tip')
+  //   .offset([-10, 0])
+  //   .direction('n')
+  //   .html(function(d) {
+  //     return "test"//nameById.get(d.id) + "<br/>Income change: " + (rateById.get(d.id)*100).toFixed(2) + "%" +
+  //     //"<br/>Population change: " + (popById.get(d.id)*100).toFixed(2) + "%" 
+  //  });
+      
+  // svg.call(tip);
+  
+  var airportMap = {},
+    extant = [],
+    flights = crossfilter(),
     all = flights.groupAll(),
     flight_date = flights.dimension(function (d) {
        return formatDate.parse(d.properties.date); 
       }),
-    flight_dates = flight_date.group();
-
+    flight_dates = flight_date.group(),
+    charts=[];
+  function zoomed() {
+      projection.translate(d3.event.translate).scale(d3.event.scale);
+      svg.selectAll("path").attr("d", path);
+  }
   var parseFligths = function (d) {
     var features = d.features;
     for (var index in features) {
@@ -61,9 +86,8 @@ $(function () {
   }
   queue()
     .defer(d3.json, "json/flights.geo.json")
-    .defer(d3.json, "json/states_land.topo.json")
-    .defer(d3.json, "json/airports2.topo.json")
-    .defer(d3.json, "json/us_airports.topojson")
+    .defer(d3.json, "json/states_land.topo.json")    
+    .defer(d3.json, "data/airports.topojson")
     .awaitAll(loaded);
 
 
@@ -72,58 +96,116 @@ $(function () {
     var fligthsJson = results[0];
     var statesJson = results[1];
     var airportsJson = results[2];
-    var usairportsJson = results[3];
+    
     
     parseFligths(fligthsJson);
 
-
-    svg.append("g")
+    
+    
+    countries = svg.append("g")
       .attr("class", "countries")
       .selectAll("path")
       .data(topojson.feature(statesJson, statesJson.objects.states_land).features)
       .enter()
       .append("path")
-      .attr("d", path);
+      .attr("d", path)
+      .on("click", clickState);;
 
-    svg.append("g")
+    airports = svg.append("g")
       .attr("class", "airports")
       .selectAll("path")
-      .data(topojson.feature(airportsJson, airportsJson.objects.airports).features)
+      .data(topojson.feature(airportsJson, airportsJson.objects.collection).features)
       .enter()
       .append("path")
-      .attr("id", function (d) { return d.id; })
+      .attr("id", function (d) { return d.properties.id; })
       .attr("d", path);
-
-    var charts = [
-
+    planes = svg.append("g")
+      .attr("class", "planes");
+    routes = svg.append("g")
+      .attr("class", "routes");
+    charts = [
       barChart(false)
         .dimension(flight_date)
         .group(flight_dates)
         .x(d3.time.scale()
-          .domain([formatDate.parse('2017-01-01'),formatDate.parse('2017-12-31') ])
-          .range([50, currentWidth-50]))
+            .domain([formatDate.parse('2017-01-01'), formatDate.parse('2017-12-31')])
+            .range([50, currentWidth - 50]))
           
           // .rangeRound([0, 10 * 365]))
         //.filter([new Date(2017, 1, 1), new Date(2017, 2, 1)])
 
     ];
-
     var chart = d3.selectAll(".chart")
       .data(charts)
       .each(function (chart) { chart.on("brush", renderAll).on("brushend", renderAll); });
 
-
-    var geos = topojson.feature(airportsJson, airportsJson.objects.airports).features;
-
-    
-    for (i in geos) {
-      airportMap[geos[i].id] = geos[i].geometry.coordinates;
-    }
+   
     renderAll();
 
-    //launchMockAnimation();
 
-    launchPlannedAnimation(fligthsJson.features);
+    function clicked(d) {
+      var centroid = path.centroid(d),
+          translate = projection.translate();
+    
+      projection.translate([
+        translate[0] - centroid[0] + width / 2,
+        translate[1] - centroid[1] + height / 2
+      ]);
+    
+      zoom.translate(projection.translate());
+    
+      svg.selectAll("path").transition()
+          .duration(700)
+          .attr("d", path);
+    }
+    
+    
+  //  launchPlannedAnimation(fligthsJson.features);
+    
+    function clickState(d) {
+      var x, y, k;
+    
+      if (d && centered !== d) {
+        var centroid = path.centroid(d);
+        x = centroid[0];
+        y = centroid[1];
+        k = 4;
+        centered = d;
+      } else {
+        x = width / 2;
+        y = height / 2;
+        k = 1;
+        centered = null;
+      }
+      routes.selectAll("path")
+          .classed("active", centered && function(d) { return d === centered; });
+      planes.selectAll("path")
+          .classed("active", centered && function(d) { return d === centered; });
+      countries.selectAll("path")
+          .classed("active", centered && function(d) { return d === centered; });      
+      airports.selectAll("path")
+          .classed("active", centered && function(d) { return d === centered; });
+    
+      planes.transition()
+          .duration(750)
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+          .style("stroke-width", 1.5 / k + "px");
+      
+      routes.transition()
+          .duration(750)
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")");
+      
+      countries.transition()
+          .duration(750)
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+          .style("stroke-width", 1.5 / k + "px");
+      
+      airports.transition()
+          .duration(750)
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+          .style("stroke-width", 1.5 / k + "px");
+          
+    }
     // barChart
     function barChart(percent) {
       if (!barChart.id) barChart.id = 0;
@@ -230,8 +312,7 @@ $(function () {
             d;
           while (++i < n) {
             d = groups[i];
-            xAxys= x(d.key);
-            path.push("M", xAxys, ",", height, "V", y(d.value), "h9V", height);
+            path.push("M", x(d.key), ",", height, "V", y(d.value), "h9V", height);
           }
           return path.join("");
         }
@@ -287,7 +368,7 @@ $(function () {
         //        return null; 
         //     } 
         // });
-        console.log('brush.chart: '+JSON.stringify(selected))
+       // console.log('brush.chart: ' + JSON.stringify(selected))
       });
 
       brush.on("brushend.chart", function () {
@@ -296,7 +377,7 @@ $(function () {
           div.select(".title a").style("display", "none");
           div.select("#clip-" + id + " rect").attr("x", null).attr("width", "100%");
           dimension.filterAll();
-        }else{
+        } else {
           var g = d3.select(this.parentNode),
           extent = brush.extent();
         if (round) g.select(".brush")
@@ -314,9 +395,9 @@ $(function () {
         dimension.filterRange(extent).top(Infinity).forEach(function (d) {
           selected.push(d)
         });
-
-        executeFlightPlans(selected);
-        console.log('brush.end: '+JSON.stringify(selected))
+          removeAllFligths();
+          executeFlightPlans(selected);
+         // console.log('brush.end: ' + JSON.stringify(selected))
         }
       });
 
@@ -382,12 +463,12 @@ $(function () {
       chart.each(render);
     }
   
-    function launchPlannedAnimation(fligthFeatures){
+    function launchPlannedAnimation(fligthFeatures) {
       //remove running animation
       executeFlightPlans(fligthFeatures);
 
     }
-    function launchMockAnimation(){
+    function launchMockAnimation() {
       setInterval(function () {
         if (isRunning) {
           var regularPoints = [];
@@ -428,16 +509,20 @@ $(function () {
   
   }
 
-  function removeAllFligths(){
-
+  function removeAllFligths() {
+    planes.selectAll("path").remove();
+    routes.selectAll("path").remove();
   }
-  function transition(plane, route) {
+  function transition(plane, route, fligth) {
     var l = route.node().getTotalLength();
     plane.transition()
       .duration(l * 100)
       .attrTween("transform", delta(plane, route.node()))
-      .each("end", function () { route.remove(); })
+      .each("end", function () { 
+        route.remove();
+       })
       .remove();
+      console.log("transition: "+JSON.stringify(fligth.properties));
   }
 
   function delta(plane, path) {
@@ -466,44 +551,48 @@ $(function () {
   
 
   /*Execute animation over fligthPlan animation */
-  function executeFlightPlans(selectedPlans){
+  function executeFlightPlans(selectedPlans) {
     
     for (var i = 0; i < selectedPlans.length; i++) {
       (function (i) {
         //to unblock thread
         setTimeout(function () {
           executeFlightPlan(selectedPlans[i])
-        }, 200*i);
+        }, 200 * i);
       })(i);
     };
       
   }
   
-  function executeFlightPlan(fligthPlanFeature){
+  function executeFlightPlan(fligthPlanFeature) {
        
 
-    var route = svg.append("path").datum(fligthPlanFeature.geometry).attr("class", "route").attr("d", path);
-    var plane = svg.append("path")
+    var route = routes.append("path").datum(fligthPlanFeature.geometry).attr("class", "route").attr("d", path);
+    var plane = planes.append("path").attr("class", "plane")
     .attr("d", "m25.21488,3.93375c-0.44355,0 -0.84275,0.18332 -1.17933,0.51592c-0.33397,0.33267 -0.61055,0.80884 -0.84275,1.40377c-0.45922,1.18911 -0.74362,2.85964 -0.89755,4.86085c-0.15655,1.99729 -0.18263,4.32223 -0.11741,6.81118c-5.51835,2.26427 -16.7116,6.93857 -17.60916,7.98223c-1.19759,1.38937 -0.81143,2.98095 -0.32874,4.03902l18.39971,-3.74549c0.38616,4.88048 0.94192,9.7138 1.42461,13.50099c-1.80032,0.52703 -5.1609,1.56679 -5.85232,2.21255c-0.95496,0.88711 -0.95496,3.75718 -0.95496,3.75718l7.53,-0.61316c0.17743,1.23545 0.28701,1.95767 0.28701,1.95767l0.01304,0.06557l0.06002,0l0.13829,0l0.0574,0l0.01043,-0.06557c0,0 0.11218,-0.72222 0.28961,-1.95767l7.53164,0.61316c0,0 0,-2.87006 -0.95496,-3.75718c-0.69044,-0.64577 -4.05363,-1.68813 -5.85133,-2.21516c0.48009,-3.77545 1.03061,-8.58921 1.42198,-13.45404l18.18207,3.70115c0.48009,-1.05806 0.86881,-2.64965 -0.32617,-4.03902c-0.88969,-1.03062 -11.81147,-5.60054 -17.39409,-7.89352c0.06524,-2.52287 0.04175,-4.88024 -0.1148,-6.89989l0,-0.00476c-0.15655,-1.99844 -0.44094,-3.6683 -0.90277,-4.8561c-0.22699,-0.59493 -0.50356,-1.07111 -0.83754,-1.40377c-0.33658,-0.3326 -0.73578,-0.51592 -1.18194,-0.51592l0,0l-0.00001,0l0,0z");
     //.on('mouseover', geomniFlyDetails)
     //.on('mouseout',removeGeomniFlyDetails);
 
     
 
-    transition(plane, route);
+    transition(plane, route,fligthPlanFeature);
   }
   function geomniFlyDetails() {
     alert("Fly: GEOM-01\nShoots Number: 1500");
   }
   function removeGeomniFlyDetails() {
-    window.alert = function() {};
+    window.alert = function () { };
   }
   /*Create fligth data for bars*/
 
+  window.stopFlightAnimations = function(){
+    removeAllFligths();
+  }
   window.filter = function (filters) {
     filters.forEach(function (d, i) { 
       charts[i].filter(d);
      });
+     removeAllFligths();
     renderAll();
   };
 
@@ -511,6 +600,7 @@ $(function () {
     charts.forEach(function (c) {
       c.filter(null);
     })
+    removeAllFligths();
     renderAll();
     svg.attr("class", "counties")
       .selectAll("path")
